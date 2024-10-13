091d605a56aa7dc1497ca46732015d9559c5a643
Sushanth-Arunachalam <60148115+Sushanth-Arunachalam@users.noreply.github.com>
2024-10-13 14:14:56 -0700

    Create train.sh

---
+++ scripts/IntrA/train.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+## run the training
+python train_cls.py \
+--dataset_name IntrA \
+--experiment_name IntrA \
+--num_classes 2 \
+--k_eig_list 153 64 16 \
+--lr 3e-4 \
+--batch_size 3 \
+--weight_decay 0.05 \
+--smoothing 0.1 \
+--augment_data \
