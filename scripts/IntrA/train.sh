cfbd267a82f15a2ab72fb26e5241deb7adfc52a3
Sushanth-Arunachalam <60148115+Sushanth-Arunachalam@users.noreply.github.com>
2024-11-12 13:06:19 -0500

    hj

--- scripts/IntrA/train.sh
+++ scripts/IntrA/train.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
 ## run the training
-python train_cls.py \
+python train_seg.py \
 --dataset_name IntrA \
 --experiment_name IntrA \
---num_classes 4 \
---k_eig_list 153 64 16 \
+--mode train \
+--num_classes 2 \
+--k_eig_list 140 64 16 \
 --lr 3e-4 \
---batch_size 3 \
---split_size 16 \
---weight_decay 0.05 \
+--batch_size 4 \
+--weight_decay 0.3 \
 --smoothing 0.1 \
 --augment_data \
+--random_rotate_axis y \
+--warm_up_epochs 20 \
+--epochs 3000 \
+--iter_num 20 \
--- scripts/IntrA/train_cls.sh
+++ scripts/IntrA/train_cls.sh
@@ -4,15 +4,11 @@
 python train_seg.py \
 --dataset_name IntrA \
 --experiment_name IntrA \
---mode train \
---num_classes 2 \
---k_eig_list 751 64 16 \
+--num_classes 1 \
+--k_eig_list 153 64 16 \
 --lr 3e-4 \
 --batch_size 3 \
---weight_decay 0.3 \
+--split_size 16 \
+--weight_decay 0.05 \
 --smoothing 0.1 \
 --augment_data \
---random_rotate_axis y \
---warm_up_epochs 20 \
---epochs 3000 \
---iter_num 20 \
