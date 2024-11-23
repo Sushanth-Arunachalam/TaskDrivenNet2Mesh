d722f08bb19d27b574ed24a8bf8923dd9264bc4c
Sushanth-Arunachalam <60148115+Sushanth-Arunachalam@users.noreply.github.com>
2024-11-23 14:59:19 -0500

    Update train.sh

--- scripts/IntrA/train.sh
+++ scripts/IntrA/train.sh
@@ -5,10 +5,10 @@ python train_seg.py \
 --dataset_name IntrA \
 --experiment_name IntrA \
 --mode train \
---num_classes 1 \
---k_eig_list 493 64 16 \
+--num_classes 2 \
+--k_eig_list 550 64 16 \
 --lr 3e-4 \
---batch_size 2 \
+--batch_size 3 \
 --weight_decay 0.3 \
 --smoothing 0.1 \
 --augment_data \
